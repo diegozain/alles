8928693d3fb79a4820dc9b9ddfa3bb3c6c099738
diegozain <diego.domenzain@protonmail.com>
2023-04-11 23:21:44 +0200

    a

--- projects/wave-proc/stft/README.md
+++ projects/wave-proc/stft/README.md
@@ -1,4 +1,4 @@
-# Short time Fourier transform 
+# 🎹 short time Fourier transform
 diego domenzain
 September 2020 @ Colorado School of Mines
 
@@ -7,6 +7,38 @@ September 2020 @ Colorado School of Mines
 Find the frequencies that make up a signal at each time.
 
 __This script is an example of finding the STFT of a signal.__
+
+´´´matlab
+% larger alpha -> narrower window
+% larger width_ -> wider window
+
+[stft_, gau_, fspect, tspect] = stft(wvlet, dt, t, alph, width_);
+
+% 🎨
+
+figure;
+plot(t,wvlet)
+axis square;
+grid on;
+xlabel('Time (sec)')
+ylabel('Data (data)')
+
+subplot(1,3,2)
+fancy_imagesc(abs(stft_),t,fspect)
+axis square;
+xlabel('Time (t)')
+ylabel('Frequency (1/t)')
+title('Power (dB)')
+simple_figure()
+
+subplot(1,3,3)
+fancy_imagesc(gau_.',t,tspect)
+axis square;
+ylabel('Time spectogram (t)')
+xlabel('Time (t)')
+title('Time window (data)')
+simple_figure()
+´´´
 
 ---
 
--- src/matlab/stft.m
+++ src/matlab/stft.m
@@ -1,4 +1,4 @@
-function [stft_, f, gau_] = stft(x, dt, t, alph, width_)
+function [stft_, gau_, fspect, tspect] = stft(x, dt, t, alph, width_)
 % ------------------------------------------------------------------------------
 % diego domenzain
 % circa 2019
@@ -9,20 +9,20 @@ nt= numel(x);
 % ------------------------------------------------------------------------------
 nt_=2^nextpow2(nt)+fix(nt*0.1);
 df = 1/dt/nt_;
-f  = (1:ceil(nt_/2)-1)*df;
+fspect  = (1:ceil(nt_/2)-1)*df;
 
 nt_extra = nt_-nt;
 x = [x; zeros(nt_extra,1)];
-t = [t ; ((t(nt)+dt) : dt : (dt*nt_extra+t(nt))).'];
+tspect = [t ; ((t(nt)+dt) : dt : (dt*nt_extra+t(nt))).'];
 % ------------------------------------------------------------------------------
 gau_ = zeros(nt,numel(x));
-nf   = numel(f);
+nf   = numel(fspect);
 stft_= zeros(nf, nt);
 for il = 1:nt
 
     lo = t(il);
 
-    gau = exp( -alph * ( ( ( t - lo ) ./ width_ ).^2 ) );
+    gau = exp( -alph * ( ( ( tspect - lo ) ./ width_ ).^8 ) );
 
     x_ = x.*gau;
 
